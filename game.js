341a13de9872569a4dd6f4f0ffeea5e497870c2c
Steven P. Kennedy <umbrylax@gmail.com>
2019-08-16 16:58:58 -0600

    base game init

--- game.js
+++ game.js
@@ -1,39 +1,90 @@
 let boss = {
   health: 100,
   name: 'Galaga',
-  hits: 0
+  hits: 0,
+  items: []
+}
+let actions = {
+  piercing: { name: 'Pierce', mod: 1, description: 'Weapons modified to pierce hull\'s shields' },
+  overdrive: { name: 'Overdrive', mod: 5, description: 'Weapons Charged to maximum power' },
+  burst: { name: 'Maxim Burst', mod: 10, description: 'Ultimate Weapon is ready to go' }
+}
+let x = ''
+let modifier = 0
+
+function useaction() {
+
+  switch (x) {
+    case 'pierce':
+      return actions.piercing.mod
+      break;
+    case 'drive':
+
+      return actions.overdrive.mod
+      break;
+    case 'burst':
+      return actions.burst.mod
+      break;
+    default:
+      return 0
+  }
+
 }
 
+
+
+//have action mods apply to the boss using a number randomizer and if statement
+function bossaction() {
+
+}
+
+function healthcheck() {
+  if (boss.health <= 0) {
+    boss.health = 0
+  }
+  return boss.health
+}
 function laser() {
-  boss.health = (boss.health - 1)
+  let modifier = useaction()
+  let ttldmg = 1 + modifier
+  boss.health = (boss.health - ttldmg)
   boss.hits = (boss.hits + 1)
+
   return boss.health,
     boss.hits,
     update()
 }
 function missle() {
 
-  boss.health = (boss.health - 5)
+  let modifier = useaction()
+  let ttldmg = 5 + modifier
+  boss.health = (boss.health - ttldmg)
   boss.hits = (boss.hits + 1)
+
   return boss.health,
     boss.hits,
     update()
 }
 function barrage() {
-  boss.health = (boss.health - 10)
+  let modifier = useaction()
+  let ttldmg = 10 + modifier
+  boss.health = (boss.health - ttldmg)
   boss.hits = (boss.hits + 1)
+
   return boss.health,
     boss.hits,
 
     update()
 }
 function update() {
+  healthcheck()
+
   let n = boss.health.toString()
-  let x = boss.hits.toString()
+  let y = boss.hits.toString()
 
   return document.getElementById('bosshealth').innerText = n,
     document.getElementById('bossname').innerText = boss.name,
-    document.getElementById('hits').innerText = x
+    document.getElementById('hits').innerText = y
 }
 
 update()
